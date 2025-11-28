a0736eff52785b9aefc74c927a3c1d5680fe547a
Anthony Jeswin A <cse22.anthonyjeswin@petengg.ac.in>
2025-11-28 19:20:34 +0530

    server.js
    Removed unused firebase-admin import from server.js

--- server.js
+++ server.js
@@ -6,8 +6,6 @@ const axios = require('axios');
 
 // Firebase setup
 require('dotenv').config();
-const admin = require('firebase-admin');
-
 const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
 
 admin.initializeApp({
